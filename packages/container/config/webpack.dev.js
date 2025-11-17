6e329b19551557151b7a9d933d076039073532b1
devansh05 <devansh2393@gmail.com>
2025-11-17 15:55:23 +0530

    Production webpack setup for container

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const htmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common.js");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
@@ -24,9 +23,6 @@ const devConfig = {
       },
       exposes: {},
       shared: packageJson.dependencies,
-    }),
-    new htmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common.js");
+const packageJson = require("../package.json");
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js", //for caching purpose
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container", // not used anywhere but good to have
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`, //telling where to find the remote container
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
