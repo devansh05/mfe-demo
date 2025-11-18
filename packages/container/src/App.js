663260f1606203c3106bab42ebcaff8671339c08
devansh05 <devansh2393@gmail.com>
2025-11-19 01:53:23 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/header";
 import MarketingApp from "./components/marketing-app";
 export default () => {
   return (
-    <div>
-      CONTAINER APP
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <hr />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
